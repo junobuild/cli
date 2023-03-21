1b9b5905a6ef0bf5b806112ee75c27bdf8b5cc8c
David Dal Busco <david.dalbusco@outlook.com>
2023-03-21 19:05:16 +0100

    build: bump open v9.0.0 with esm

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,7 +1,7 @@
 import {config} from 'dotenv';
 import esbuild from 'esbuild';
-import {existsSync, mkdirSync, readFileSync, writeFileSync} from 'fs';
-import {dirname, extname, join} from 'path';
+import {existsSync, mkdirSync, writeFileSync} from 'fs';
+import {join} from 'path';
 
 config({
   path: process.env.NODE_ENV === 'development' ? `.env.development` : `.env.production`
@@ -21,30 +21,6 @@ if (!existsSync(dist)) {
   mkdirSync(dist);
 }
 
-// issue: https://github.com/sindresorhus/open/issues/302
-// source workaround: https://github.com/evanw/esbuild/issues/859
-const nodeModulesOpen = /\/node_modules\/open\//;
-
-const dirnamePlugin = {
-  name: 'dirname',
-  setup(build) {
-    build.onLoad({filter: /.*/}, ({path: filePath}) => {
-      if (filePath.match(nodeModulesOpen)) {
-        const contents = readFileSync(filePath, 'utf8');
-        const loader = extname(filePath).substring(1);
-        const _dirname = dirname(filePath);
-
-        return {
-          contents: contents
-            .replaceAll('__dirname', `"${_dirname}"`)
-            .replaceAll('__filename', `"${filePath}"`),
-          loader
-        };
-      }
-    });
-  }
-};
-
 const script = await esbuild.build({
   entryPoints: ['src/index.ts'],
   outfile: 'dist/index.mjs',
@@ -56,8 +32,7 @@ const script = await esbuild.build({
   banner: {
     js: "import { createRequire as topLevelCreateRequire } from 'module';\n const require = topLevelCreateRequire(import.meta.url);"
   },
-  define,
-  plugins: [dirnamePlugin]
+  define
 });
 
 writeFileSync('dist/index.js', `#!/usr/bin/env node\n${script.outputFiles[0].text}`);
