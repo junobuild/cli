beee917431c89f4538d44fb9cf90704436bdd301
David Dal Busco <david.dalbusco@outlook.com>
2023-05-25 15:57:59 +0200

    feat: remove checks for running cli in actions in which config is empty

--- src/utils/satellite.utils.ts
+++ src/utils/satellite.utils.ts
@@ -2,24 +2,14 @@ import {SatelliteParameters} from '@junobuild/admin';
 import {actorParameters} from './actor.utils';
 import {getAuthSatellites} from './auth.config.utils';
 
-export const satelliteParameters = (satelliteId: string): SatelliteParameters => {
-  const satellites = getAuthSatellites();
-
-  if (!satellites.map(({p}) => p).includes(satelliteId)) {
-    throw new Error(
-      'You are not a controller of the satellite. Are you logged in with the expected profile? Are you in the root directory of your project?'
-    );
-  }
-
-  return {
-    satelliteId,
-    ...actorParameters()
-  };
-};
+export const satelliteParameters = (satelliteId: string): SatelliteParameters => ({
+  satelliteId,
+  ...actorParameters()
+});
 
 /**
  * For display purpose, use either the name or id. Most probably we should find a name but for simplicity reason we fallback to Id.
- * @param satellite name or id
+ * @param satelliteId name or id
  */
 export const satelliteKey = (satelliteId: string): string => {
   const satellites = getAuthSatellites();
