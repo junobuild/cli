10b1d1321c3bf2e09af6faf1707bb41dfef9c29a
David Dal Busco <david.dalbusco@outlook.com>
2023-10-07 20:54:57 +0200

    chore: repo transferred to new org junobuild

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -13,4 +13,4 @@ export const NODE_LTS = 18;
 export const CONSOLE_URL = 'https://console.juno.build';
 export const CONSOLE_SATELLITE_URL = `${CONSOLE_URL}/satellite/?s=`;
 export const JUNO_CDN_URL = 'https://fmkjf-bqaaa-aaaal-acpza-cai.raw.icp0.io';
-export const GITHUB_API_CLI_URL = 'https://api.github.com/repos/buildwithjuno/cli';
+export const GITHUB_API_CLI_URL = 'https://api.github.com/repos/junobuild/cli';
