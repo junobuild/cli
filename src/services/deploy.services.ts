a8a3a09690eacb2ac0800eaf2619c8311ba6c27c
David Dal Busco <david.dalbusco@outlook.com>
2023-12-22 13:22:32 +0100

    chore: lint

--- src/services/deploy.services.ts
+++ src/services/deploy.services.ts
@@ -3,7 +3,7 @@ import {yellow} from 'kleur';
 import {compare} from 'semver';
 import {readSatelliteConfig} from '../configs/juno.config';
 import {MEMORY_HEAP_WARNING, MEMORY_SIZE_ENDPOINT_VERSION} from '../constants/deploy.constants';
-import {confirmAndExit} from '../utils/prompt.utils';
+import {NEW_CMD_LINE, confirmAndExit} from '../utils/prompt.utils';
 import {satelliteParameters} from '../utils/satellite.utils';
 
 export const assertSatelliteMemorySize = async () => {
@@ -19,7 +19,7 @@ export const assertSatelliteMemorySize = async () => {
     console.log(
       `Your satellite (${yellow(
         `v${currentVersion}`
-      )}) is not up-to-date, and the memory size cannot be verified.`
+      )}) is not up-to-date, and the memory size cannot be verified.${NEW_CMD_LINE}`
     );
     return;
   }
@@ -31,8 +31,10 @@ export const assertSatelliteMemorySize = async () => {
   }
 
   await confirmAndExit(
-    `⚠️  Your satellite's heap memory is ${
+    `⚠️  Your satellite's heap memory is ${Intl.NumberFormat('en-US', {
+      maximumSignificantDigits: 3
+    }).format(
       Number(heap) / 1_000_000
-    } MB. It is approaching 1 GB, which is the recommended upper limit. Are you sure you want to deploy?`
+    )} MB. It is approaching 1 GB, which is the recommended upper limit. Are you sure you want to deploy?`
   );
 };
