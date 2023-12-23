1164ff58c6b3adc47f19898cc954438963c26486
David Dal Busco <david.dalbusco@outlook.com>
2023-12-23 12:32:39 +0100

    feat: use non raw domain and custom domain name for the cdn (#80)

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -11,6 +11,6 @@ export const COLLECTION_DAPP = '#dapp';
 export const NODE_18 = 18;
 export const CONSOLE_URL = 'https://console.juno.build';
 export const CONSOLE_SATELLITE_URL = `${CONSOLE_URL}/satellite/?s=`;
-export const JUNO_CDN_URL = 'https://fmkjf-bqaaa-aaaal-acpza-cai.raw.icp0.io';
+export const JUNO_CDN_URL = 'https://cdn.juno.build';
 export const GITHUB_API_CLI_URL = 'https://api.github.com/repos/junobuild/cli';
 export const IGNORE_OS_FILES = ['.ds_store', 'thumbs.db'];
