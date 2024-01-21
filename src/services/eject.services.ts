5e4ec008df2b96a8d9bc9f0f29ccc1a573efd869
David Dal Busco <david.dalbusco@outlook.com>
2024-01-21 11:10:03 +0100

    feat: ask for overwrite

--- src/services/eject.services.ts
+++ src/services/eject.services.ts
@@ -1,11 +1,17 @@
+import {yellow} from 'kleur';
+import {existsSync} from 'node:fs';
 import {copyFile, mkdir} from 'node:fs/promises';
-import {dirname, join} from 'node:path';
-import ora from 'ora';
+import {dirname, join, relative} from 'node:path';
 import {fileURLToPath} from 'url';
 import {checkRustVersion} from '../utils/env.utils';
+import {confirm} from '../utils/prompt.utils';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+const TEMPLATE_PATH = '../templates/eject';
+const TEMPLATE_SATELLITE_PATH = join(TEMPLATE_PATH, 'src/satellite');
+const DESTINATION_SATELLITE_PATH = './src/satellite';
 
 export const eject = async () => {
   const {valid} = await checkRustVersion();
@@ -14,25 +20,49 @@ export const eject = async () => {
     return;
   }
 
-  const spinner = ora(`Ejecting satellite...`).start();
+  await writeFile({
+    template: 'Cargo.toml',
+    sourceFolder: TEMPLATE_PATH,
+    destinationFolder: '.'
+  });
 
-  try {
-    await copyFile(
-      join(__dirname, '../templates/eject/Cargo.toml'),
-      join(process.cwd(), './Cargo.toml')
+  await mkdir(join(process.cwd(), './src/satellite/src'), {recursive: true});
+
+  await writeFile({
+    template: 'Cargo.toml',
+    sourceFolder: TEMPLATE_SATELLITE_PATH,
+    destinationFolder: DESTINATION_SATELLITE_PATH
+  });
+
+  await writeFile({
+    template: 'lib.rs',
+    sourceFolder: join(TEMPLATE_SATELLITE_PATH, 'src'),
+    destinationFolder: join(DESTINATION_SATELLITE_PATH, 'src')
+  });
+};
+
+const writeFile = async ({
+  sourceFolder,
+  destinationFolder,
+  template
+}: {
+  sourceFolder: string;
+  destinationFolder: string;
+  template: string;
+}) => {
+  const destination = join(process.cwd(), destinationFolder, template);
+
+  if (existsSync(destination)) {
+    const answer = await confirm(
+      `File ${yellow(
+        relative(process.cwd(), destination)
+      )} already exists. Do you want to overwrite it?`
     );
 
-    await mkdir(join(process.cwd(), './src/satellite/src'), {recursive: true});
-
-    await copyFile(
-      join(__dirname, '../templates/eject/src/satellite/Cargo.toml'),
-      join(process.cwd(), './src/satellite/Cargo.toml')
-    );
-    await copyFile(
-      join(__dirname, '../templates/eject/src/satellite/src/lib.rs'),
-      join(process.cwd(), './src/satellite/src/lib.rs')
-    );
-  } finally {
-    spinner.stop();
+    if (!answer) {
+      return;
+    }
   }
+
+  await copyFile(join(__dirname, sourceFolder, template), destination);
 };
