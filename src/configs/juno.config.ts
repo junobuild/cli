e287d0bdf2738d25e357461192437a91c14a68c9
David Dal Busco <david.dalbusco@outlook.com>
2024-02-11 09:08:31 +0100

    feat: experiment loading ts and js

--- src/configs/juno.config.ts
+++ src/configs/juno.config.ts
@@ -1,6 +1,11 @@
+import fs from 'fs';
 import {access, readFile, writeFile} from 'node:fs/promises';
+import {dirname, join} from 'node:path';
+import { ModuleKind, ModuleResolutionKind, ScriptTarget, transpileModule } from "typescript";
+import {fileURLToPath} from 'url';
 import {JUNO_JSON} from '../constants/constants';
 import type {JunoConfig, OrbiterConfig, SatelliteConfig} from '../types/juno.config';
+import { nodeRequire } from "./node.utils";
 
 export const saveSatelliteConfig = async (satellite: SatelliteConfig): Promise<void> => {
   if (await junoConfigExist()) {
@@ -49,7 +54,27 @@ const writeJunoConfig = async (config: JunoConfig): Promise<void> => {
   await writeFile(JUNO_JSON, JSON.stringify(config, null, 2), 'utf-8');
 };
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const readJunoConfig = async (): Promise<JunoConfig> => {
+  // const url = join(process.cwd(), 'juno.js');
+  // console.log('------------>', (await import(url)).default);
+
+  const urlTs = join(process.cwd(), 'juno.ts');
+
+  console.log(urlTs);
+
+  const result = nodeRequire(urlTs);
+
+  console.log("Export", result.module.default);
+
+  // const source = "let x: string  = 'string'";
+  //
+  // let result = transpileModule(source, { compilerOptions: { module: ModuleKind.CommonJS }});
+  //
+  // console.log(JSON.stringify(result));
+
   const buffer = await readFile(JUNO_JSON);
   return JSON.parse(buffer.toString('utf-8'));
 };
---
+++ src/configs/node.utils.ts
@@ -0,0 +1,71 @@
+import ts from 'typescript';
+
+/**
+ * Source: Stencil
+ * https://github.com/ionic-team/stencil/blob/main/src/compiler/sys/node-require.ts
+ */
+export const nodeRequire = (id: string) => {
+  const results = {
+    module: undefined as any,
+    id,
+  };
+
+  try {
+    const fs: typeof import('fs') = require('fs');
+    const path: typeof import('path') = require('path');
+
+    results.id = path.resolve(id);
+
+    // ensure we cleared out node's internal require() cache for this file
+    delete require.cache[results.id];
+
+    // let's override node's require for a second
+    // don't worry, we'll revert this when we're done
+    require.extensions['.ts'] = (module: NodeJS.Module, fileName: string) => {
+      let sourceText = fs.readFileSync(fileName, 'utf8');
+
+      if (fileName.endsWith('.ts')) {
+        // looks like we've got a typed config file
+        // let's transpile it to .js quick
+        const tsResults = ts.transpileModule(sourceText, {
+          fileName,
+          compilerOptions: {
+            module: ts.ModuleKind.CommonJS,
+            moduleResolution: ts.ModuleResolutionKind.NodeJs,
+            esModuleInterop: true,
+            target: ts.ScriptTarget.ESNext,
+            allowJs: true,
+          },
+        });
+        sourceText = tsResults.outputText;
+      } else {
+        // quick hack to turn a modern es module
+        // into and old school commonjs module
+        sourceText = sourceText.replace(/export\s+\w+\s+(\w+)/gm, 'exports.$1');
+      }
+
+      try {
+        // we need to coerce because of the requirements for the arguments to
+        // this function. It's safe enough since it's already wrapped in a
+        // `try { } catch`.
+        (module as NodeModuleWithCompile)._compile(sourceText, fileName);
+      } catch (e: any) {
+        throw e;
+      }
+    };
+
+    // let's do this!
+    results.module = require(results.id);
+
+    // all set, let's go ahead and reset the require back to the default
+    require.extensions['.ts'] = undefined;
+  } catch (e: any) {
+    throw e;
+  }
+
+  return results;
+};
+
+interface NodeModuleWithCompile extends NodeModule {
+  _compile(code: string, filename: string): any;
+}
