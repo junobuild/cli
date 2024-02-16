628d374aed228ee2575f2c939907085edfb2db6d
David Dal Busco <david.dalbusco@outlook.com>
2024-02-16 14:18:56 +0100

    feat: subcommand in one word

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -127,7 +127,7 @@ export const helpDevContinue = `${helpDevBuild}
 export const helpDev = `
 ${TITLE}
 
-Usage: ${green('juno')} ${cyan('dev')} ${magenta('<sub-command>')}
+Usage: ${green('juno')} ${cyan('dev')} ${magenta('<subcommand>')}
 
 Subcommands:
   ${helpDevBuild}
