0e1f8dd924b3e376f44c8fc93c294c95753ee564
David Dal Busco <david.dalbusco@outlook.com>
2024-06-15 13:47:55 +0200

    feat: use cli-tools to deploy

--- src/commands/deploy.ts
+++ src/commands/deploy.ts
@@ -1,15 +1,9 @@
-import type {FileDetails} from '@junobuild/cli-tools';
-import {fullPath, hasArgs, prepareDeploy} from '@junobuild/cli-tools';
-import {uploadBlob, type Asset, type AssetKey} from '@junobuild/core-peer';
-import {isNullish, nonNullish} from '@junobuild/utils';
-import {Blob} from 'buffer';
+import type {UploadFileStorage} from '@junobuild/cli-tools';
+import {deploy as cliDeploy, hasArgs} from '@junobuild/cli-tools';
+import {uploadBlob, type Asset} from '@junobuild/core-peer';
 import {red} from 'kleur';
-import Listr from 'listr';
 import {lstatSync} from 'node:fs';
-import {readFile} from 'node:fs/promises';
-import {basename, relative} from 'node:path';
 import {junoConfigExist, readJunoConfig} from '../configs/juno.config';
-import {COLLECTION_DAPP, UPLOAD_BATCH_SIZE} from '../constants/constants';
 import {clear} from '../services/clear.services';
 import {assertSatelliteMemorySize, listAssets} from '../services/deploy.services';
 import {links} from '../services/links.services';
@@ -43,65 +37,39 @@ const executeDeploy = async (args?: string[]) => {
       }
     });
 
-  const {files: sourceFiles, sourceAbsolutePath} = await prepareDeploy({
-    config: satelliteConfig,
-    listAssets: listExistingAssets,
-    assertSourceDirExists
-  });
-
-  if (sourceFiles.length === 0) {
-    console.log('No new files to upload.');
-    return;
-  }
-
-  await assertSatelliteMemorySize(args);
+  const assertMemory = async () => {
+    await assertSatelliteMemorySize(args);
+  };
 
   const satellite = satelliteParameters({satellite: satelliteConfig, env});
 
-  const fileDetailsPath = (file: FileDetails): string => file.alternateFile ?? file.file;
-
-  const upload = async (file: FileDetails): Promise<AssetKey> => {
-    const filePath = fileDetailsPath(file);
-
-    return await uploadBlob({
+  const uploadFile = async ({
+    filename,
+    fullPath,
+    data,
+    collection,
+    headers,
+    encoding
+  }: UploadFileStorage) => {
+    await uploadBlob({
       satellite,
-      filename: basename(filePath),
-      fullPath: fullPath({file: filePath, sourceAbsolutePath}),
+      filename,
+      fullPath,
       // @ts-expect-error type incompatibility NodeJS vs bundle
-      data: new Blob([await readFile(file.file)]),
-      collection: COLLECTION_DAPP,
-      headers: [
-        ...(file.mime === undefined
-          ? []
-          : ([['Content-Type', file.mime]] as Array<[string, string]>))
-      ],
-      encoding: file.encoding
+      data,
+      collection,
+      headers,
+      encoding
     });
   };
 
-  const uploadFiles = async (groupFiles: FileDetails[]) => {
-    // Execute upload UPLOAD_BATCH_SIZE files at a time max preventively to not stress too much the network
-    for (let i = 0; i < groupFiles.length; i += UPLOAD_BATCH_SIZE) {
-      const files = groupFiles.slice(i, i + UPLOAD_BATCH_SIZE);
-
-      const tasks = new Listr<AssetKey>(
-        files.map((file) => ({
-          title: `Uploading ${relative(sourceAbsolutePath, file.file)}`,
-          task: async () => await upload(file)
-        })),
-        {concurrent: true}
-      );
-
-      await tasks.run();
-    }
-  };
-
-  // TODO: temporary possible race condition fix until Satellite v0.0.13 is published
-  // We must upload the alternative path first to ensure . Friday Oct. 10 2023 I got unexpected race condition while uploading the Astro sample example (file hoisted.8961d9b1.js).
-  await uploadFiles(sourceFiles.filter(({alternateFile}) => nonNullish(alternateFile)));
-  await uploadFiles(sourceFiles.filter(({alternateFile}) => isNullish(alternateFile)));
-
-  console.log(`\n🚀 Deploy complete!`);
+  await cliDeploy({
+    config: satelliteConfig,
+    listAssets: listExistingAssets,
+    assertSourceDirExists,
+    assertMemory,
+    uploadFile
+  });
 };
 
 const assertSourceDirExists = (source: string) => {
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -7,8 +7,6 @@ export const DAPP_COLLECTION = '#dapp';
 export const SATELLITE_WASM_NAME = 'satellite';
 export const MISSION_CONTROL_WASM_NAME = 'mission_control';
 export const ORBITER_WASM_NAME = 'orbiter';
-export const UPLOAD_BATCH_SIZE = 20;
-export const COLLECTION_DAPP = '#dapp';
 export const NODE_18 = 18;
 export const CONSOLE_URL = 'https://console.juno.build';
 export const CONSOLE_SATELLITE_URL = `${CONSOLE_URL}/satellite/?s=`;
