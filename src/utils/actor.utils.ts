178ffe8c769baca2bf4ec7038fac8713a6be3794
David Dal Busco <david.dalbusco@outlook.com>
2024-07-20 20:25:50 +0200

    feat: remove node-fetch (#129)

--- src/utils/actor.utils.ts
+++ src/utils/actor.utils.ts
@@ -2,7 +2,6 @@ import {Ed25519KeyIdentity} from '@dfinity/identity/lib/cjs/identity/ed25519';
 import {type ActorParameters} from '@junobuild/admin';
 import {isNullish, nonNullish} from '@junobuild/utils';
 import {red} from 'kleur';
-import fetch from 'node-fetch';
 import {getToken} from '../configs/cli.config';
 import {REVOKED_CONTROLLERS} from '../constants/constants';
 import {getProcessToken} from './process.utils';
@@ -23,8 +22,6 @@ export const actorParameters = (): ActorParameters => {
 
   return {
     identity,
-    // TODO: TypeScript incompatibility window.fetch vs nodejs.fetch vs agent-ts using typeof fetch
-    // @ts-expect-error
     fetch,
     ...(nonNullish(process.env.CONTAINER_URL) && {container: process.env.CONTAINER_URL})
   };
