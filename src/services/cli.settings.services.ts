d0635255b172ff3e2f368246f524bab49d34a2a3
David Dal Busco <david.dalbusco@outlook.com>
2024-07-28 20:39:36 +0200

    chore: more generic text

--- src/services/cli.settings.services.ts
+++ src/services/cli.settings.services.ts
@@ -2,7 +2,7 @@ import {assertAnswerCtrlC} from '@junobuild/cli-tools';
 import prompts from 'prompts';
 
 export const askForPassword = async (
-  message = 'What is your CLI configuration password?'
+  message = 'Please provide the password for your CLI configuration.'
 ): Promise<string> => {
   const {encryptionKey}: {encryptionKey: string} = await prompts([
     {
