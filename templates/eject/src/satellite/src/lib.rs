3077752b8ee6e8b80239134f244ff45fc90d1693
David Dal Busco <david.dalbusco@outlook.com>
2024-11-17 17:55:29 +0100

    feat: Satellite v0.0.21

--- templates/eject/src/satellite/src/lib.rs
+++ templates/eject/src/satellite/src/lib.rs
@@ -1,11 +1,12 @@
 use junobuild_macros::{
     assert_delete_asset, assert_delete_doc, assert_set_doc, assert_upload_asset, on_delete_asset,
-    on_delete_doc, on_delete_many_assets, on_delete_many_docs, on_set_doc, on_set_many_docs,
-    on_upload_asset,
+    on_delete_doc, on_delete_filtered_assets, on_delete_filtered_docs, on_delete_many_assets,
+    on_delete_many_docs, on_set_doc, on_set_many_docs, on_upload_asset,
 };
 use junobuild_satellite::{
     include_satellite, AssertDeleteAssetContext, AssertDeleteDocContext, AssertSetDocContext,
-    AssertUploadAssetContext, OnDeleteAssetContext, OnDeleteDocContext, OnDeleteManyAssetsContext,
+    AssertUploadAssetContext, OnDeleteAssetContext, OnDeleteDocContext,
+    OnDeleteFilteredAssetsContext, OnDeleteFilteredDocsContext, OnDeleteManyAssetsContext,
     OnDeleteManyDocsContext, OnSetDocContext, OnSetManyDocsContext, OnUploadAssetContext,
 };
 
@@ -29,6 +30,11 @@ async fn on_delete_many_docs(_context: OnDeleteManyDocsContext) -> Result<(), St
     Ok(())
 }
 
+#[on_delete_filtered_docs]
+async fn on_delete_filtered_docs(_context: OnDeleteFilteredDocsContext) -> Result<(), String> {
+    Ok(())
+}
+
 #[on_upload_asset]
 async fn on_upload_asset(_context: OnUploadAssetContext) -> Result<(), String> {
     Ok(())
@@ -44,6 +50,10 @@ async fn on_delete_many_assets(_context: OnDeleteManyAssetsContext) -> Result<()
     Ok(())
 }
 
+#[on_delete_filtered_assets]
+async fn on_delete_filtered_assets(_context: OnDeleteFilteredAssetsContext) -> Result<(), String> {
+    Ok(())
+}
 #[assert_set_doc]
 fn assert_set_doc(_context: AssertSetDocContext) -> Result<(), String> {
     Ok(())
