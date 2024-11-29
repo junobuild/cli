0f4771723c091a21a8126d9ee57330dd01f2388e
David Dal Busco <david.dalbusco@outlook.com>
2024-11-29 09:27:19 +0100

    chore: fmt

--- src/help/upgrade.help.ts
+++ src/help/upgrade.help.ts
@@ -1,14 +1,14 @@
-import {cyan, green, magenta, yellow} from 'kleur';
+import {cyan, green, yellow} from 'kleur';
 import {helpMode, helpOutput} from './common.help';
 import {TITLE} from './help';
-import { TARGET_OPTION_NOTE, targetOption } from "./target.help";
+import {TARGET_OPTION_NOTE, targetOption} from './target.help';
 
 export const UPGRADE_DESCRIPTION = 'Upgrade a module to a new version.';
 
 const usage = `Usage: ${green('juno')} ${cyan('upgrade')} ${yellow('[options]')}
 
 Options:
-  ${targetOption("upgrade")}  
+  ${targetOption('upgrade')}  
   ${yellow('-s, --src')}             An optional local gzipped WASM file for the upgrade. By default, the CDN will be used.
   ${yellow('-r, --reset')}           Reset to the initial state.
   ${yellow('-cc, --clear-chunks')}   Clear any previously uploaded WASM chunks (applies if the WASM size is greater than 2MB).
