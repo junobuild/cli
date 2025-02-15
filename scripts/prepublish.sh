44eedfced5b0127f5fb19904082e01e4377a70e6
David Dal Busco <david.dalbusco@outlook.com>
2025-02-15 15:30:53 +0100

    build: prepublishOnly script to clean-up template eject

---
+++ scripts/prepublish.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+rm -rf ./templates/eject/src/declarations
+rm -rf ./templates/eject/target
+
+rm -f ./templates/eject/src/satellite/satellite_extension.did
+rm -f ./templates/eject/Cargo.lock
