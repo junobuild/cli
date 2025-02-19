660e14809dcb4f669d69c8bb6e50eea1ffe4db3b
David Dal Busco <david.dalbusco@outlook.com>
2025-02-19 17:49:38 +0100

    feat: juno init --minimal

--- src/types/config.ts
+++ src/types/config.ts
@@ -9,3 +9,7 @@ export type JunoConfigWithSatelliteId = Omit<JunoConfig, 'satellite' | 'orbiter'
   satellite: Omit<SatelliteConfig, 'id' | 'satellitesIds'> & Required<Pick<SatelliteConfig, 'id'>>;
   orbiter?: Omit<OrbiterConfig, 'orbiterId'>;
 };
+
+export type JunoConfigWithPlaceholder = Omit<JunoConfig, 'satellite' | 'orbiter'> & {
+  satellite: Pick<SatelliteConfig, 'source'>;
+};
