76f27953aab278021146b252fff3c4641eb71bce
David Dal Busco <david.dalbusco@outlook.com>
2025-02-21 07:35:14 +0100

    feat: add option to check only the version of the cli (#188)

--- src/help/version.help.ts
+++ src/help/version.help.ts
@@ -1,7 +1,30 @@
-import {logHelpWithMode} from './generic.help';
+import {cyan, green, yellow} from 'kleur';
+import {helpMode, helpOutput} from './common.help';
+import {TITLE} from './help';
 
-export const VERSION_DESCRIPTION = 'Check the version of the modules and cli.';
+export const VERSION_DESCRIPTION = 'Check the version of the modules and CLI.';
+
+const usage = `Usage: ${green('juno')} ${cyan('init')} ${yellow('[options]')}
+
+Options:
+  ${yellow('-c, --cli')}         Check only the version of the CLI.
+  ${helpMode}
+  ${yellow('-h, --help')}            Output usage information.`;
+
+const doc = `${VERSION_DESCRIPTION}
+
+\`\`\`bash
+${usage}
+\`\`\`
+`;
+
+const help = `${TITLE}
+
+${VERSION_DESCRIPTION}
+
+${usage}
+`;
 
 export const logHelpVersion = (args?: string[]) => {
-  logHelpWithMode({args, command: 'version', description: VERSION_DESCRIPTION});
+  console.log(helpOutput(args) === 'doc' ? doc : help);
 };
