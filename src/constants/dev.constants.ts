2ed858a3809df8255ca573b043c4a1ff8a4d71d7
David Dal Busco <david.dalbusco@outlook.com>
2025-02-28 07:02:46 +0100

    fix: minimal version of ic-wasm is v0.8.5

--- src/constants/dev.constants.ts
+++ src/constants/dev.constants.ts
@@ -12,5 +12,5 @@ export const TEMPLATE_PATH = '../templates/eject';
 export const TEMPLATE_SATELLITE_PATH = join(TEMPLATE_PATH, 'src', 'satellite');
 
 export const RUST_MIN_VERSION = '1.70.0';
-export const IC_WASM_MIN_VERSION = '0.3.6';
+export const IC_WASM_MIN_VERSION = '0.8.5';
 export const DOCKER_MIN_VERSION = '24.0.0';
