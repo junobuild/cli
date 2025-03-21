a0c25dcc60e720bd096daea62986a908e861c8ba
David Dal Busco <david.dalbusco@outlook.com>
2025-03-21 16:02:27 +0100

    feat: install lib and tsconfig

--- src/services/eject/eject.javascript.services.ts
+++ src/services/eject/eject.javascript.services.ts
@@ -1,4 +1,6 @@
-import {mkdir} from 'node:fs/promises';
+import {execute} from '@junobuild/cli-tools';
+import {magenta} from 'kleur';
+import {mkdir, readFile} from 'node:fs/promises';
 import {join} from 'node:path';
 import {
   DEVELOPER_PROJECT_SATELLITE_PATH,
@@ -8,6 +10,8 @@ import {
   TS_TEMPLATE_PATH
 } from '../../constants/dev.constants';
 import {copyTemplateFile} from '../../utils/fs.utils';
+import {detectPackageManager} from '../../utils/pm.utils';
+import {confirmAndExit} from '../../utils/prompt.utils';
 
 export const ejectTypeScript = async () => {
   await eject({lang: 'ts'});
@@ -18,13 +22,67 @@ export const ejectJavaScript = async () => {
 };
 
 const eject = async ({lang}: {lang: 'ts' | 'mjs'}) => {
-  const devProjectSrcPath = join(DEVELOPER_PROJECT_SATELLITE_PATH);
+  await installFunctionsLib();
 
-  await mkdir(devProjectSrcPath, {recursive: true});
+  await createTargetDir();
 
+  await copyTemplateIndex({lang});
+
+  if (lang === 'ts') {
+    await copyTemplateTsConfig();
+  }
+};
+
+const copyTemplateIndex = async ({lang}: {lang: 'ts' | 'mjs'}) => {
   await copyTemplateFile({
     template: lang === 'mjs' ? INDEX_MJS : INDEX_TS,
     sourceFolder: lang === 'mjs' ? MJS_TEMPLATE_PATH : TS_TEMPLATE_PATH,
     destinationFolder: DEVELOPER_PROJECT_SATELLITE_PATH
   });
+};
+
+const copyTemplateTsConfig = async () => {
+  await copyTemplateFile({
+    template: 'tsconfig.json',
+    sourceFolder: TS_TEMPLATE_PATH,
+    destinationFolder: DEVELOPER_PROJECT_SATELLITE_PATH
+  });
+};
+
+const createTargetDir = async () => {
+  const devProjectSrcPath = join(DEVELOPER_PROJECT_SATELLITE_PATH);
+  await mkdir(devProjectSrcPath, {recursive: true});
+};
+
+const installFunctionsLib = async () => {
+  const functionsAlreadyInstalled = await hasFunctionsLib();
+
+  if (functionsAlreadyInstalled) {
+    return;
+  }
+
+  await confirmAndExit(
+    `The ${magenta(
+      '@junobuild/functions'
+    )} library is required to develop serverless functions. Install it now?`
+  );
+
+  const pm = detectPackageManager();
+
+  await execute({
+    command: pm ?? 'npm',
+    args: [pm === 'npm' ? 'i' : 'add', '@junobuild/functions']
+  });
+};
+
+const hasFunctionsLib = async (): Promise<boolean> => {
+  try {
+    const packageJson = await readFile(join(process.cwd(), 'package.json'), 'utf-8');
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-type-assertion
+    const {dependencies} = JSON.parse(packageJson) as {dependencies?: Record<string, string>};
+    return Object.keys(dependencies ?? {}).includes('@junobuild/functions');
+  } catch (_err: unknown) {
+    // This should not block the developer therefore we fallback to asking for installing the library.
+    return false;
+  }
 };
