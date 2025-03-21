9f36b27130417f33beef979ee18cbf68389b4565
David Dal Busco <david.dalbusco@outlook.com>
2025-03-21 16:26:19 +0100

    chore: fmt and lint

--- src/services/eject/eject.services.ts
+++ src/services/eject/eject.services.ts
@@ -43,7 +43,6 @@ const ejectWithCmdLang = async ({lang}: {lang: string | undefined}) => {
 const promptLangAndEject = async () => {
   const {lang} = await selectLang();
 
-  // eslint-disable-next-line @typescript-eslint/switch-exhaustiveness-check
   switch (lang) {
     case 'rs':
       await ejectRust();
@@ -54,9 +53,6 @@ const promptLangAndEject = async () => {
     case 'mjs':
       await ejectJavaScript();
       break;
-    default:
-      unsupportedLang();
-      return;
   }
 
   console.log(success());
