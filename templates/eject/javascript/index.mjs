490997f7d90795d66abb7655b674bf29b9dfa65a
David Dal Busco <david.dalbusco@outlook.com>
2025-03-29 19:30:30 +0100

    feat: add all hooks and assertions to Sputnik templates (#218)

--- templates/eject/javascript/index.mjs
+++ templates/eject/javascript/index.mjs
@@ -4,12 +4,67 @@ import {defineAssert, defineHook} from '@junobuild/functions';
 // However, if you don’t have to implement all of them, for example to improve readability or reduce unnecessary logic,
 // you can selectively delete the features you do not need.
 
+export const onSetDoc = defineHook({
+  collections: [],
+  run: async (context) => {}
+});
+
+export const onSetManyDocs = defineHook({
+  collections: [],
+  run: async (context) => {}
+});
+
+export const onDeleteDoc = defineHook({
+  collections: [],
+  run: async (context) => {}
+});
+
+export const onDeleteManyDocs = defineHook({
+  collections: [],
+  run: async (context) => {}
+});
+
+export const onDeleteFilteredDocs = defineHook({
+  collections: [],
+  run: async (context) => {}
+});
+
+export const onUploadAsset = defineHook({
+  collections: [],
+  run: async (context) => {}
+});
+
+export const onDeleteAsset = defineHook({
+  collections: [],
+  run: async (context) => {}
+});
+
+export const onDeleteManyAssets = defineHook({
+  collections: [],
+  run: async (context) => {}
+});
+
+export const onDeleteFilteredAssets = defineHook({
+  collections: [],
+  run: async (context) => {}
+});
+
 export const assertSetDoc = defineAssert({
   collections: [],
   assert: (context) => {}
 });
 
-export const onSetDoc = defineHook({
+export const assertDeleteDoc = defineAssert({
   collections: [],
-  run: async (context) => {}
+  assert: (context) => {}
+});
+
+export const assertUploadAsset = defineAssert({
+  collections: [],
+  assert: (context) => {}
+});
+
+export const assertDeleteAsset = defineAssert({
+  collections: [],
+  assert: (context) => {}
 });
