5d440e572b77c90f6ecb0cd46c66621f79287d14
David Dal Busco <david.dalbusco@outlook.com>
2025-05-20 18:22:02 +0200

    fix: revert memory check guard removal

--- src/constants/deploy.constants.ts
+++ src/constants/deploy.constants.ts
@@ -1,3 +1,4 @@
+export const MEMORY_SIZE_ENDPOINT_VERSION = '0.0.14';
 export const MEMORY_HEAP_WARNING = 900_000_000n;
 
 // Observed empirical value, the threshold might be higher, but at the time I wrote this, pagination there were a bit more than 700 files deployed on the Juno website without any issues.
