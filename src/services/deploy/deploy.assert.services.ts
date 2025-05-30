6a9dc7b9e78c9246ad54bb42fa0cb9e63a5b9350
David Dal Busco <david.dalbusco@outlook.com>
2025-05-30 08:44:37 +0200

    refactor: extract execute deploy to module

--- src/services/deploy/deploy.assert.services.ts
+++ src/services/deploy/deploy.assert.services.ts
@@ -1,19 +1,11 @@
 import {satelliteMemorySize, satelliteVersion} from '@junobuild/admin';
-import type {Asset} from '@junobuild/core';
-import {listAssets as listAssetsLib} from '@junobuild/core';
 import {yellow} from 'kleur';
 import {compare} from 'semver';
-import {readJunoConfig} from '../configs/juno.config';
-import {DAPP_COLLECTION} from '../constants/constants';
-import {
-  DEPLOY_LIST_ASSETS_PAGINATION,
-  MEMORY_HEAP_WARNING,
-  MEMORY_SIZE_ENDPOINT_VERSION
-} from '../constants/deploy.constants';
-import type {SatelliteConfigEnv} from '../types/config';
-import {configEnv} from '../utils/config.utils';
-import {NEW_CMD_LINE, confirmAndExit} from '../utils/prompt.utils';
-import {satelliteParameters} from '../utils/satellite.utils';
+import {readJunoConfig} from '../../configs/juno.config';
+import {MEMORY_HEAP_WARNING, MEMORY_SIZE_ENDPOINT_VERSION} from '../../constants/deploy.constants';
+import {configEnv} from '../../utils/config.utils';
+import {NEW_CMD_LINE, confirmAndExit} from '../../utils/prompt.utils';
+import {satelliteParameters} from '../../utils/satellite.utils';
 
 export const assertSatelliteMemorySize = async (args?: string[]) => {
   const env = configEnv(args);
@@ -63,42 +55,4 @@ export const assertSatelliteMemorySize = async (args?: string[]) => {
       maxMemorySize
     )} MB. Are you sure you want to proceed with the deployment?`
   );
-};
-
-export const listAssets = async ({
-  startAfter,
-  env
-}: {
-  startAfter?: string;
-  env: SatelliteConfigEnv;
-}): Promise<Asset[]> => {
-  const {assets, items_page, matches_pages} = await listAssetsLib({
-    collection: DAPP_COLLECTION,
-    satellite: await satelliteParameters(env),
-    filter: {
-      order: {
-        desc: true,
-        field: 'keys'
-      },
-      paginate: {
-        startAfter,
-        limit: DEPLOY_LIST_ASSETS_PAGINATION
-      }
-    }
-  });
-
-  const last = <T>(elements: T[]): T | undefined => {
-    const {length, [length - 1]: last} = elements;
-    return last;
-  };
-
-  if ((items_page ?? 0n) < (matches_pages ?? 0n)) {
-    const nextAssets = await listAssets({
-      startAfter: last(assets)?.fullPath,
-      env
-    });
-    return [...assets, ...nextAssets];
-  }
-
-  return assets;
 };
