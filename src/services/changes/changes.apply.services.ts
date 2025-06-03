1f6f3be9b3f9a80106026c5a129eaaf0565833c9
David Dal Busco <david.dalbusco@outlook.com>
2025-06-03 12:21:17 +0200

    feat: spare space

--- src/services/changes/changes.apply.services.ts
+++ src/services/changes/changes.apply.services.ts
@@ -43,7 +43,7 @@ const executeApplyChanges = async ({
 
     spinner.stop();
 
-    console.log(`\n🎯 Change ID ${proposalId} applied.`);
+    console.log(`🎯 Change ID ${proposalId} applied.`);
   } catch (err: unknown) {
     spinner.stop();
 
