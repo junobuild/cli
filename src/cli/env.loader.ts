de5df9538c3ffb2ce69f4f3dc5cf002be4042cfa
David Dal Busco <david.dalbusco@outlook.com>
2025-06-05 12:42:31 +0200

    refactor: extract loader

--- src/cli/env.loader.ts
+++ src/cli/env.loader.ts
@@ -1,27 +1,33 @@
 import {nextArg} from '@junobuild/cli-tools';
-import {JunoCliEnv} from '../types/cli.env';
+import {JunoCliEnv, JunoConsole} from '../types/cli.env';
 
 export const loadEnv = (): JunoCliEnv => {
   const [_, ...args] = process.argv.slice(2);
 
   const mode = nextArg({args, option: '-m'}) ?? nextArg({args, option: '--mode'});
   const containerUrl = nextArg({args, option: '--container-url'});
-  const consoleUrl = nextArg({args, option: '--console-url'});
 
   const envContainerUrl =
     containerUrl ?? (mode === 'development' ? 'http://127.0.0.1:5987' : undefined);
-  const envConsoleUrl =
-    consoleUrl ?? (mode === 'development' ? 'http://localhost:5866' : 'https://console.juno.build');
 
   return {
     mode: mode ?? 'production',
     containerUrl: envContainerUrl,
-    console: {
-      urls: {
-        root: envConsoleUrl,
-        satellite: `${envConsoleUrl}/satellite/?s=`,
-        auth: `${envConsoleUrl}/cli`
-      }
+    console: loadEnvConsole({args, mode})
+  };
+};
+
+const loadEnvConsole = ({args, mode}: {args?: string[]; mode: string | undefined}): JunoConsole => {
+  const consoleUrl = nextArg({args, option: '--console-url'});
+
+  const envConsoleUrl =
+    consoleUrl ?? (mode === 'development' ? 'http://localhost:5866' : 'https://console.juno.build');
+
+  return {
+    urls: {
+      root: envConsoleUrl,
+      satellite: `${envConsoleUrl}/satellite/?s=`,
+      auth: `${envConsoleUrl}/cli`
     }
   };
 };
