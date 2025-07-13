9f1d306b9c1ad0158ad4cf652e749f2c6d99c892
David Dal Busco <david.dalbusco@outlook.com>
2025-07-13 16:45:47 +0200

    feat: tips login mode on no token found

--- src/api/actor.api.ts
+++ src/api/actor.api.ts
@@ -1,10 +1,11 @@
 import {Ed25519KeyIdentity} from '@dfinity/identity/lib/cjs/identity/ed25519';
 import {isNullish, nonNullish} from '@dfinity/utils';
 import {type ActorParameters} from '@junobuild/admin';
-import {red} from 'kleur';
+import {green, red} from 'kleur';
 import {getToken} from '../configs/cli.config';
 import {ENV} from '../env';
 import {getProcessToken} from '../utils/process.utils';
+import {NEW_CMD_LINE} from '../utils/prompt.utils';
 import {initAgent} from './agent.api';
 
 export const actorParameters = async (): Promise<
@@ -13,7 +14,14 @@ export const actorParameters = async (): Promise<
   const token = getProcessToken() ?? (await getToken());
 
   if (isNullish(token)) {
-    console.log(`${red('No access key found.')} Are you logged in?`);
+    console.log(`${red(`No access key found for ${ENV.mode}.`)} Are you logged in?`);
+
+    if (ENV.mode !== 'production') {
+      console.log(
+        `${NEW_CMD_LINE}💡 To enforce separation of concerns and security, each mode requires a separate login — for example: ${green(`juno login --mode ${ENV.mode}`)}`
+      );
+    }
+
     process.exit(1);
   }
 
