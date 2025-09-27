37b0130373192055f1c5ce7822573431877d2579
David Dal Busco <david.dalbusco@outlook.com>
2025-09-27 11:10:29 +0200

    feat: support for enabling or disabled network services of the emulator (#375)
    * feat: support for enabling or disabled network services of the emulator
    
    * feat: move default in docker
    
    * feat: pass the all network
    
    * feat: config update

--- src/services/emulator/_runner.services.ts
+++ src/services/emulator/_runner.services.ts
@@ -213,6 +213,8 @@ const startEmulator = async ({config: extendedConfig}: {config: CliEmulatorConfi
 
   const platform = config.runner?.platform;
 
+  const network = config?.network;
+
   await execute({
     command: runner,
     args: [
@@ -230,6 +232,7 @@ const startEmulator = async ({config: extendedConfig}: {config: CliEmulatorConfi
             `${config.skylab.ports?.console ?? EMULATOR_SKYLAB.ports.console}:${EMULATOR_PORT_CONSOLE}`
           ]
         : []),
+      ...(nonNullish(network) ? ['-e', `NETWORK=${JSON.stringify(network)}`] : []),
       '-v',
       `${volume}:/juno/.juno`,
       ...(nonNullish(configFile) && nonNullish(configFilePath)
