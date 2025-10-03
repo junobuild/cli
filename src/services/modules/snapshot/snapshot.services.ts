e082a014c69c5ab7204ff2632eba28e0e7da7084
David Dal Busco <david.dalbusco@outlook.com>
2025-10-03 14:01:27 +0200

    feat: target another module when uploading snapshot (#385)
    * feat: target another module when uploading snapshot
    
    * fix: canister id to resolve snapshot

--- src/services/modules/snapshot/snapshot.services.ts
+++ src/services/modules/snapshot/snapshot.services.ts
@@ -1,8 +1,16 @@
 import type {snapshot_id} from '@dfinity/ic-management';
 import {encodeSnapshotId} from '@dfinity/ic-management';
 import {Principal} from '@dfinity/principal';
+import {nonNullish, notEmptyString} from '@dfinity/utils';
+import {findJunoPackageDependency, getJunoPackage} from '@junobuild/admin';
 import {nextArg} from '@junobuild/cli-tools';
+import {
+  JUNO_PACKAGE_MISSION_CONTROL_ID,
+  JUNO_PACKAGE_ORBITER_ID,
+  JUNO_PACKAGE_SATELLITE_ID
+} from '@junobuild/config';
 import ora from 'ora';
+import {actorParameters} from '../../../api/actor.api';
 import {
   deleteCanisterSnapshot,
   loadCanisterSnapshot,
@@ -126,10 +134,54 @@ export const uploadSnapshot = async ({
   segment: AssetKey;
   args?: string[];
 }) => {
-  const canisterId = Principal.fromText(cId);
-
   const folder = nextArg({args, option: '--dir'});
   assertNonNullishFolderExists(folder);
+
+  const targetCanisterId = nextArg({args, option: '--target-id'});
+
+  if (notEmptyString(targetCanisterId)) {
+    // TODO: extract into a service we can reuse for upgrade as well
+    const actorParams = await actorParameters();
+
+    const pkg = await getJunoPackage({
+      moduleId: targetCanisterId,
+      ...actorParams
+    });
+
+    const validJunoPackage = (): {valid: boolean} => {
+      if (segment === 'mission_control') {
+        return {valid: pkg?.name === JUNO_PACKAGE_MISSION_CONTROL_ID};
+      }
+
+      if (segment === 'orbiter') {
+        return {valid: pkg?.name === JUNO_PACKAGE_ORBITER_ID};
+      }
+
+      // It's stock
+      if (pkg?.name === JUNO_PACKAGE_SATELLITE_ID) {
+        return {valid: true};
+      }
+
+      const {dependencies} = pkg ?? {dependencies: {}};
+
+      const satelliteDependency = findJunoPackageDependency({
+        dependencies,
+        dependencyId: JUNO_PACKAGE_SATELLITE_ID
+      });
+
+      return {valid: nonNullish(satelliteDependency)};
+    };
+
+    const {valid} = validJunoPackage();
+
+    if (!valid) {
+      await confirmAndExit(
+        `⚠️  The selected target is not a ${displaySegment(segment)}; this may cause issues if restored later. Are you sure you want to continue?`
+      );
+    }
+  }
+
+  const canisterId = Principal.fromText(notEmptyString(targetCanisterId) ? targetCanisterId : cId);
 
   const {snapshotId} = await loadSnapshotAndAssertOverwrite({canisterId, segment});
 
