e1ca5af5b4b409f8fc727ac22ab972ae0d977cba
David Dal Busco <david.dalbusco@outlook.com>
2025-10-03 16:51:14 +0200

    chore: setup (e2e) tests

---
+++ playwright.config.ts
@@ -0,0 +1,36 @@
+import {defineConfig, devices} from '@playwright/test';
+
+const DEV = (process.env.NODE_ENV ?? 'production') === 'development';
+
+export default defineConfig({
+  testDir: './tests',
+  snapshotDir: `./${DEV ? 'tmp' : 'src'}/e2e/snapshots`,
+  testMatch: ['**/*.e2e.ts', '**/*.spec.ts'],
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  workers: process.env.CI ? 1 : undefined,
+  expect: {
+    toHaveScreenshot: {
+      animations: 'disabled',
+      caret: 'hide'
+    }
+  },
+  webServer: {
+    command: 'npm run build',
+    url: 'http://localhost:5866',
+    reuseExistingServer: DEV,
+    timeout: 120 * 1000
+  },
+  use: {
+    testIdAttribute: 'data-tid',
+    baseURL: 'http://localhost:5866',
+    trace: 'on',
+    ...(DEV && {headless: false})
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: {...devices['Desktop Chrome']}
+    }
+  ]
+});
---
+++ tests/constants/test-ids.constants.ts
@@ -0,0 +1,18 @@
+import {TestIds} from '../types/test-id';
+
+export const testIds: TestIds = {
+  auth: {
+    signIn: 'btn-sign-in'
+  },
+  createSatellite: {
+    launch: 'btn-launch-satellite',
+    create: 'btn-create-satellite',
+    input: 'input-satellite-name',
+    website: 'input-radio-satellite-website',
+    application: 'input-radio-satellite-application',
+    continue: 'btn-continue-overview'
+  },
+  satelliteOverview: {
+    visit: 'link-visit-satellite'
+  }
+};
