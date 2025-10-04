0bba4ac8ca75df06f622825827793561be85947a
David Dal Busco <david.dalbusco@outlook.com>
2025-10-04 09:06:39 +0200

    feat: wait for visibility

--- tests/page-objects/console.page.ts
+++ tests/page-objects/console.page.ts
@@ -46,7 +46,9 @@ export class ConsolePage extends IdentityPage {
 
     await this.page.getByTestId(testIds.createSatellite.create).click();
 
-    await expect(this.page.getByTestId(testIds.createSatellite.continue)).toBeVisible();
+    await expect(this.page.getByTestId(testIds.createSatellite.continue)).toBeVisible({
+      timeout: 20000
+    });
 
     await this.page.getByTestId(testIds.createSatellite.continue).click();
   }
