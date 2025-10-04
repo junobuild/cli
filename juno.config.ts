2187c0a98c33d6b6c18b3f1927c197d9600a2d68
David Dal Busco <david.dalbusco@outlook.com>
2025-10-04 08:43:50 +0200

    fix: emulator config to be tested

--- juno.config.ts
+++ juno.config.ts
@@ -3,7 +3,7 @@ import {defineConfig} from '@junobuild/config';
 export default defineConfig({
   satellite: {
     ids: {
-      development: 'jx5yt-yyaaa-aaaal-abzbq-cai',
+      development: '<DEV_SATELLITE_ID>',
       production: '<PROD_SATELLITE_ID>'
     },
     source: 'build',
