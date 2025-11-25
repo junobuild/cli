0a79b2d0c0bbc9571662b75e337f7107a4a055dc
David Dal Busco <david.dalbusco@outlook.com>
2025-11-25 09:09:48 +0100

    feat: check icp-bindgen with globall fallback

--- src/utils/build.bindgen.utils.ts
+++ src/utils/build.bindgen.utils.ts
@@ -5,15 +5,19 @@ import {checkToolInstalled} from './env.utils';
 import {detectPackageManager} from './pm.utils';
 import {confirmAndExit} from './prompt.utils';
 
-export const checkIcpBindgen = async (): Promise<{valid: boolean}> => {
-  const pm = detectPackageManager();
+export const checkIcpBindgen = async ({
+  globalFallback
+}: {
+  globalFallback: boolean;
+}): Promise<{valid: boolean}> => {
+  const {valid: localValid} = await checkLocalIcpBindgen();
 
-  const command = pm === 'npm' || isNullish(pm) ? 'npx' : pm;
+  if (localValid === true) {
+    return {valid: true};
+  }
 
-  const {valid} = await checkToolInstalled({
-    command,
-    args: ['icp-bindgen', '--version', ...(command === 'npx' ? ['--no'] : [])]
-  });
+  if (globalFallback) {
+  }
 
   if (valid === false) {
     return {valid};
@@ -33,4 +37,22 @@ export const checkIcpBindgen = async (): Promise<{valid: boolean}> => {
   }
 
   return {valid: true};
+};
+
+const checkLocalIcpBindgen = async (): Promise<{valid: boolean | 'error'}> => {
+  const pm = detectPackageManager();
+
+  const command = pm === 'npm' || isNullish(pm) ? 'npx' : pm;
+
+  return await checkToolInstalled({
+    command,
+    args: ['icp-bindgen', '--version', ...(command === 'npx' ? ['--no'] : [])]
+  });
+};
+
+const checkGlobalIcpBindgen = async (): Promise<{valid: boolean | 'error'}> => {
+  return await checkToolInstalled({
+    command: 'icp-bindgen',
+    args: ['--version']
+  });
 };
