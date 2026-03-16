20d23404d65be639f748f718b01ebada30d1623a
David Dal Busco <david.dalbusco@outlook.com>
2026-03-16 14:51:35 +0100

    feat: extract image name to derived emulator config
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/types/emulator.ts
+++ src/types/emulator.ts
@@ -6,12 +6,16 @@ export type EmulatorRunnerType = EmulatorRunner['type'];
 
 export type EmulatorConfigWithoutConsole = Exclude<EmulatorConfig, {console: EmulatorConsole}>;
 
+// eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
+export type EmulatorImage = `junobuild/${EmulatorType}:latest` | string;
+
 export interface CliEmulatorDerivedConfig {
   containerName: string;
   runner: EmulatorRunnerType;
   emulatorType: EmulatorType;
   targetDeploy: string;
   extraHosts: string[];
+  image: EmulatorImage;
 }
 
 export interface CliEmulatorConfig {
