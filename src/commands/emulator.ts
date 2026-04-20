86216c282e825cb989109f7f60d1d0c8cb667d3d
David Dal Busco <david.dalbusco@outlook.com>
2026-04-20 11:04:11 +0200

    chore: fmt
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- src/commands/emulator.ts
+++ src/commands/emulator.ts
@@ -3,10 +3,10 @@ import {logHelpEmulator} from '../help/emulator.help';
 import {logHelpEmulatorStart} from '../help/emulator.start.help';
 import {logHelpEmulatorWait} from '../help/emulator.wait.help';
 import {clear} from '../services/emulator/clear.services';
+import {pull} from '../services/emulator/pull.services';
 import {start} from '../services/emulator/start.services';
 import {stop} from '../services/emulator/stop.services';
 import {wait} from '../services/emulator/wait.services';
-import {pull} from '../services/emulator/pull.services';
 
 export const emulator = async (args?: string[]) => {
   const [subCommand] = args ?? [];
